a4caff2836472c343d8a4a56dc94b44890c90d87
souleymanethiao23 <souleymane.thiao@unchk.edu.sn>
2026-06-08 12:54:33 +0000

    Delete routes directory

--- routes/alerts.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getAlerts } = require('../controllers/alertController');
-
-router.get('/', getAlerts);
-
-module.exports = router;
